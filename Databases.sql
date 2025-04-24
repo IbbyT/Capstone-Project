66d7512679b83cdc0bf5f8310f86f8dcda282f40
FrankGeerwar-Amador <fag22@njit.edu>
2025-04-24 17:57:12 -0400

    Database Products

--- Databases.sql
+++ Databases.sql
@@ -1,38 +1,38 @@
 CREATE TABLE ComputerParts(
-    CategoryID      INT(11)         NOT NULL, 
-    CategoryCode    VARCHAR(20)     NOT NULL,
-    CategoryName    VARCHAR(255)    NOT NULL,
+    ProductID      INT(11)         NOT NULL, 
+    ProductCode    VARCHAR(20)     NOT NULL,
+    ProductName    VARCHAR(255)    NOT NULL,
     Price           FLOAT(11)       NOT NULL,
-    PRIMARY KEY(CategoryID)
+    PRIMARY KEY(ProductID)
 );
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (1, 'RAM', '32GB Storage for PC', 150.99);
 
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (2, 'RAM', '64GB Storage for PC', 200.99);
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (3, 'MotherBoard', 'Gaming MotherBoard', 230.00);
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (4, 'MotherBoard', 'Gaming and WiFI motherboard', 250.00);
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (5, 'CPU', 'Intel Core', 300.00);
 
 INSERT INTO ComputerParts
-(CategoryID, CategoryCode, CategoryName, Price)
+(ProductID, ProductCode, ProductName, Price)
 VALUES
 (6, 'CPU', 'AMD Ryzen', 330.00);
